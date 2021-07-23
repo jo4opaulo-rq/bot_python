d22dc6cd660afcf98773b3fdab00add4add4f747
jo4opaulo-rq <joaoroque317@gmail.com>
2021-07-23 02:40:33 -0300

    Bot para abrir miha playlist no spotify

---
+++ bot.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+pyautogui.PAUSE=2
+
+pyautogui.press('win')
+pyautogui.write('spotify')
+pyautogui.press('down')
+pyautogui.press('enter')
+time.sleep(5)
+pyautogui.click(x=73, y=456)
+time.sleep(1)
+pyautogui.click(x=295, y=424)
